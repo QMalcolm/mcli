81d720b7e695b9646bb46b47a23df3db7fcbc99e
T. Quigley Malcolm <tqmalcolm@protonmail.com>
2020-04-19 13:27:39 -0700

    (memory) inefficient file search

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,10 @@ struct Cli {
 
 fn main() {
     let args = Cli::from_args();
-    println!("{}", args.pattern);
-    println!("{}", args.path.to_string_lossy());
+    let content = std::fs::read_to_string(&args.path).expect("could not read file");
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
 }
